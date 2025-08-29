faee5a93f31163408f1a6a5a9b50cc620f598ba0
Ni-Guvara <niguvara@outlook.com>
2025-08-29 10:36:56 +0800

    docs: 删除乱码注释添加自动化编译脚本

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+
+rm -rf ./build/*
+cd build
+cmake ..
+make
--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -70,24 +70,29 @@ ThreadPool::~ThreadPool()
 	conExit_.wait(lock, [&]() -> bool
 				  { return threadsMap_.size() == 0; });
 }
+
 void ThreadPool::setMode(ThreadMode mode)
 {
 	this->mode_ = mode;
 }
+
 bool ThreadPool::checkThreadPoolIsRunning()
 {
 	return threadPoolIsRunning_;
 }
+
 void ThreadPool::setTaskQueThreshold(int taskQueSizeThreshold)
 {
 	if (!checkThreadPoolIsRunning())
 		this->taskQueSizeThreshold_ = taskQueSizeThreshold;
 }
+
 void ThreadPool::setTaskQueCurrentSize(int currentTasksSize)
 {
 	if (!checkThreadPoolIsRunning())
 		this->currentTaskSize_ = currentTasksSize;
 }
+
 void ThreadPool::setThreadsQueThreshold(int threadsQueSizeThreshold)
 {
 	if (!checkThreadPoolIsRunning())
@@ -102,21 +107,20 @@ void ThreadPool::setThreadQueCurrentSize(int currentThreadsSize)
 void ThreadPool::start(int threadSize)
 {
 	this->threadPoolIsRunning_ = true;
-	// ??????????
+
 	this->initThreadSize_ = threadSize;
 	this->currentThreadSize_ = threadSize;
 
-	// ??????????initThreadSize ??Thread???
 	for (int i = 0; i < this->currentThreadSize_; i++)
 	{
-		// ?????????? std::unique_ptr
+
 		auto f = std::bind(&ThreadPool::threadHandler, this, std::placeholders::_1);
-		// ???????????Thread??
+
 		std::unique_ptr<Thread> ptr = std::make_unique<Thread>(f);
-		// emplace_back????
+
 		this->threadsMap_.emplace(ptr->getId(), std::move(ptr));
 	}
-	// ???????
+
 	for (int i = 0; i < this->currentThreadSize_; i++)
 	{
 		this->threadsMap_[i]->start();
@@ -124,18 +128,16 @@ void ThreadPool::start(int threadSize)
 	}
 }
 
-// ??????
 Result ThreadPool::submitTask(std::shared_ptr<Task> task)
 {
-	std::cout << "????????" << std::endl;
-	// ?????????
+	std::cout << "Submit Task" << std::endl;
+
 	std::unique_lock<std::mutex> lock(this->taskQueMtx);
 
-	// ?????????§Ø?
 	if (!this->taskQueNotFull.wait_for(lock, std::chrono::seconds(1), [&]() -> bool
 									   { return (this->currentTaskSize_ < this->taskQueSizeThreshold_); }))
 	{
-		std::cerr << "???????????????" << std::endl;
+		std::cerr << "Task Quque is Full " << std::endl;
 		return Result(task, false);
 	}
 
@@ -143,21 +145,20 @@ Result ThreadPool::submitTask(std::shared_ptr<Task> task)
 	this->currentTaskSize_++;
 	this->taskQueNotEmpty.notify_all();
 
-	// CACHED??????????????§³???????????????????????§³???????????????????????
 	if (mode_ == ThreadMode::CACHED)
 	{
 		if (this->currentTaskSize_ > this->idleThreadsSize_ &&
 			this->currentThreadSize_ < this->threadsQueSizeThreshold_)
 		{
-			// ?????
+
 			auto f = std::bind(&ThreadPool::threadHandler, this, std::placeholders::_1);
-			// ???? std::unique_ptr??????????????????Thread????????
+
 			std::unique_ptr<Thread> ptr = std::make_unique<Thread>(f);
 			int id = ptr->getId();
 			std::cout << "Thread " << id << "????" << std::endl;
-			// ?????????thread?????????????????????
+
 			this->threadsMap_.emplace(id, std::move(ptr));
-			// ?????????????
+
 			this->threadsMap_[id]->start();
 			this->currentThreadSize_++;
 			this->idleThreadsSize_++;
@@ -176,10 +177,10 @@ void ThreadPool::threadHandler(int threadId)
 	{
 		std::shared_ptr<Task> task;
 		{
-			std::cout << "Thread " << threadId << "!!!" << std::endl;
-			// ???????
+			std::cout << "Thread " << threadId << std::endl;
+
 			std::unique_lock<std::mutex> lock(this->taskQueMtx);
-			// ??????????
+
 			while (currentTaskSize_ == 0)
 			{
 				if (!threadPoolIsRunning_)
@@ -190,20 +191,19 @@ void ThreadPool::threadHandler(int threadId)
 					return;
 				}
 
-				// CACHED ??????????????????????60s???????????????
 				if (this->mode_ == ThreadMode::CACHED)
 				{
-					// ????§Ø????
+
 					if (std::cv_status::timeout == taskQueNotEmpty.wait_for(lock, std::chrono::seconds(1)))
 					{
 						auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - last);
 						if (duration.count() > THREAD_IDLE_DURATION && currentThreadSize_ > this->initThreadSize_)
 						{
 							std::cout << "Thread  " << threadId << "exit!!!";
-							threadsMap_.erase(threadId); // ??????
+							threadsMap_.erase(threadId);
 							this->currentThreadSize_--;
 							this->idleThreadsSize_--;
-							// ????
+
 							return;
 						}
 					}
@@ -214,7 +214,6 @@ void ThreadPool::threadHandler(int threadId)
 				}
 			}
 
-			// ????????????
 			task = this->tasksQue_.front();
 			this->tasksQue_.pop();
 			this->currentTaskSize_--;
@@ -222,13 +221,10 @@ void ThreadPool::threadHandler(int threadId)
 
 			if (this->currentTaskSize_ > 0)
 				taskQueNotEmpty.notify_all();
-
-			// ?????????????????????????
 			taskQueNotFull.notify_all();
 		}
-		std::cout << "Thread " << threadId << "!!!" << std::endl;
+		std::cout << "Thread " << threadId << std::endl;
 
-		// ???????
 		if (task != nullptr)
 			task->exec();
 
